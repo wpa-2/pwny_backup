dc571e7b04faf260bba7084601db785282b47897
wpa-2 <9049886+wpa-2@users.noreply.github.com>
2024-09-22 16:54:35 +0100

    Add files via upload

--- autobackup.py
+++ autobackup.py
@@ -23,15 +23,11 @@ class autobackup(plugins.Plugin):
     def on_loaded(self):
         logging.info(f"AUTO-BACKUP: Full loaded configuration: {self.options}")
 
-        required_options = ['interval', 'local_backup_path', 'remote_backup']
+        required_options = ['interval', 'local_backup_path']
         for opt in required_options:
             if opt not in self.options:
-                logging.warning(f"AUTO-BACKUP: Option {opt} is not set.")
-                if opt == 'remote_backup':
-                    self.options['remote_backup'] = None
-                else:
-                    logging.error(f"AUTO-BACKUP: Required option {opt} is not set.")
-                    return
+                logging.error(f"AUTO-BACKUP: Required option {opt} is not set.")
+                return
 
         backup_interval = self.options.get('interval', 1) * 3600
         self.backup_thread = threading.Thread(target=self.schedule_backup, args=(backup_interval,), daemon=True)
@@ -46,31 +42,7 @@ class autobackup(plugins.Plugin):
 
     def perform_backup(self):
         os_arch = platform.machine()
-        if os_arch == "aarch64":
-            files_to_backup = [
-                "/root/brain.json",
-                "/root/.api-report.json",
-                "/root/handshakes/",
-                "/root/peers/",
-                "/etc/pwnagotchi/",
-                "/boot/firmware/config.txt",
-                "/boot/firmware/cmdline.txt",
-                "/usr/local/share/pwnagotchi/custom-plugins/"
-            ]
-        elif os_arch == "armv7l":
-            files_to_backup = [
-                "/root/brain.json",
-                "/root/.api-report.json",
-                "/root/handshakes/",
-                "/root/peers/",
-                "/etc/pwnagotchi/",
-                "/boot/config.txt",
-                "/boot/cmdline.txt",
-                "/usr/local/share/pwnagotchi/custom-plugins/"
-            ]
-        else:
-            logging.error("AUTO_BACKUP: Unsupported architecture detected.")
-            return
+        files_to_backup = self.get_files_to_backup(os_arch)
 
         if not os.path.exists(self.options['local_backup_path']):
             logging.info(f"AUTO_BACKUP: Local backup path does not exist. Creating: {self.options['local_backup_path']}")
@@ -86,19 +58,56 @@ class autobackup(plugins.Plugin):
             return
 
         try:
-            logging.info("AUTO_BACKUP: Backing up ...")
-            tar_command = f"tar --exclude='/etc/pwnagotchi/log/pwnagotchi.log' -czvf {local_backup_path} {' '.join(valid_files)}"
-            logging.info(f"AUTO_BACKUP: Running tar command: {tar_command}")
+            self.create_backup_archive(valid_files, local_backup_path)
+            self.handle_github_backup(local_backup_path, backup_filename)
+            self.handle_remote_backup(local_backup_path, backup_filename)
 
-            result = subprocess.run(tar_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            if result.returncode != 0:
-                logging.error(f"AUTO_BACKUP: Failed to create backup. Error: {result.stderr.decode()} Command: {tar_command}")
-                return
+        except OSError as os_e:
+            self.tries += 1
+            logging.error(f"AUTO_BACKUP: Error: {os_e}")
 
-            logging.info(f"AUTO_BACKUP: Backup created successfully at {local_backup_path}")
+    def get_files_to_backup(self, os_arch):
+        if os_arch == "aarch64":
+            return [
+                "/root/brain.json",
+                "/root/.api-report.json",
+                "/root/handshakes/",
+                "/root/peers/",
+                "/etc/pwnagotchi/",
+                "/boot/firmware/config.txt",
+                "/boot/firmware/cmdline.txt",
+                "/usr/local/share/pwnagotchi/custom-plugins/"
+            ]
+        elif os_arch == "armv7l":
+            return [
+                "/root/brain.json",
+                "/root/.api-report.json",
+                "/root/handshakes/",
+                "/root/peers/",
+                "/etc/pwnagotchi/",
+                "/boot/config.txt",
+                "/boot/cmdline.txt",
+                "/usr/local/share/pwnagotchi/custom-plugins/"
+            ]
+        else:
+            logging.error("AUTO_BACKUP: Unsupported architecture detected.")
+            return []
 
-            # GitHub integration
-            github_backup_dir = self.options.get('github_backup_dir', 'Backups')
+    def create_backup_archive(self, valid_files, local_backup_path):
+        logging.info("AUTO_BACKUP: Backing up ...")
+        tar_command = f"tar --exclude='/etc/pwnagotchi/log/pwnagotchi.log' -czvf {local_backup_path} {' '.join(valid_files)}"
+        logging.info(f"AUTO_BACKUP: Running tar command: {tar_command}")
+
+        result = subprocess.run(tar_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        if result.returncode != 0:
+            logging.error(f"AUTO_BACKUP: Failed to create backup. Error: {result.stderr.decode()} Command: {tar_command}")
+            return
+
+        logging.info(f"AUTO_BACKUP: Backup created successfully at {local_backup_path}")
+
+    def handle_github_backup(self, local_backup_path, backup_filename):
+        if 'github_repo' in self.options and 'github_backup_dir' in self.options:
+            github_backup_dir = self.options['github_backup_dir']
             github_backup_path = os.path.join(self.options['local_backup_path'], github_backup_dir)
 
             if not os.path.exists(github_backup_path):
@@ -107,49 +116,41 @@ class autobackup(plugins.Plugin):
             final_backup_path = os.path.join(github_backup_path, backup_filename)
             os.replace(local_backup_path, final_backup_path)
 
-            # Set up sparse checkout
-            sparse_checkout_dir = os.path.join(github_backup_path, '.git', 'info')
-            os.makedirs(sparse_checkout_dir, exist_ok=True)
-            with open(os.path.join(sparse_checkout_dir, 'sparse-checkout'), 'w') as sparse_file:
-                sparse_file.write(f"{github_backup_dir}/*\n")
+            # Setup Git configuration and push backup
+            self.git_setup(github_backup_path, backup_filename)
 
-            subprocess.run(f"git config core.sparseCheckout true", cwd=github_backup_path, shell=True)
+    def git_setup(self, github_backup_path, backup_filename):
+        os.makedirs(os.path.join(github_backup_path, '.git', 'info'), exist_ok=True)
+        with open(os.path.join(github_backup_path, '.git', 'info', 'sparse-checkout'), 'w') as sparse_file:
+            sparse_file.write(f"{self.options.get('github_backup_dir', 'Backups')}/*\n")
 
-            # Run Git commands
-            git_commands = [
-                f"cd {github_backup_path} && git config core.sparseCheckout true",
-                f"cd {github_backup_path} && git read-tree -mu HEAD",
-                f"cd {github_backup_path} && git add -f {backup_filename}",
-                f"cd {github_backup_path} && git commit -m 'Backup on {datetime.now()}'",
-                f"cd {github_backup_path} && git push --force origin main"
-            ]
+        subprocess.run(f"git config core.sparseCheckout true", cwd=github_backup_path, shell=True)
+        self.run_git_commands(github_backup_path, backup_filename)
 
-            for cmd in git_commands:
-                logging.info(f"AUTO_BACKUP: Running Git command as user pi: {cmd}")
-                result = subprocess.run(f"sudo -u pi bash -c \"{cmd}\"", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                logging.info(f"Git stdout: {result.stdout.decode()}")
-                logging.info(f"Git stderr: {result.stderr.decode()}")
+    def run_git_commands(self, github_backup_path, backup_filename):
+        git_commands = [
+            f"cd {github_backup_path} && git add -f {backup_filename}",
+            f"cd {github_backup_path} && git commit -m 'Backup on {datetime.now()}'",
+            f"cd {github_backup_path} && git push --force origin main"
+        ]
 
-                if result.returncode != 0:
-                    logging.error(f"AUTO_BACKUP: Git command '{cmd}' failed with exit code {result.returncode}")
-                    return
+        for cmd in git_commands:
+            logging.info(f"AUTO_BACKUP: Running Git command: {cmd}")
+            result = subprocess.run(f"sudo -u pi bash -c \"{cmd}\"", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            if result.returncode != 0:
+                logging.error(f"AUTO_BACKUP: Git command '{cmd}' failed with exit code {result.returncode}")
+                return
 
-            # Remote backup
-            if self.options.get('remote_backup'):
-                try:
-                    server_address, ssh_key = self.options['remote_backup'].split(',')
-                    rsync_command = f"rsync -avz -e 'ssh -i {ssh_key} -o StrictHostKeyChecking=no' {final_backup_path} {server_address}/"
-                    logging.info(f"AUTO_BACKUP: Sending backup to server using rsync: {rsync_command}")
-                    result = subprocess.run(rsync_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                    if result.returncode == 0:
-                        logging.info("AUTO_BACKUP: Backup successfully sent to server using rsync.")
-                    else:
-                        logging.error(f"AUTO_BACKUP: Failed to send backup to server using rsync. Error: {result.stderr.decode()}")
-                except ValueError:
-                    logging.error("AUTO_BACKUP: Incorrect remote backup configuration format.")
-                except subprocess.CalledProcessError as e:
-                    logging.error(f"AUTO_BACKUP: Failed to send backup to server using rsync. Error: {e}")
-
-        except OSError as os_e:
-            self.tries += 1
-            logging.error(f"AUTO_BACKUP: Error: {os_e}")
+    def handle_remote_backup(self, local_backup_path, backup_filename):
+        if 'remote_backup' in self.options:
+            try:
+                server_address, ssh_key = self.options['remote_backup'].split(',')
+                rsync_command = f"rsync -avz -e 'ssh -i {ssh_key} -o StrictHostKeyChecking=no' {local_backup_path} {server_address}/"
+                logging.info(f"AUTO_BACKUP: Sending backup to server using rsync: {rsync_command}")
+                result = subprocess.run(rsync_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                if result.returncode == 0:
+                    logging.info("AUTO_BACKUP: Backup successfully sent to server using rsync.")
+                else:
+                    logging.error(f"AUTO_BACKUP: Failed to send backup to server using rsync. Error: {result.stderr.decode()}")
+            except ValueError:
+                logging.error("AUTO_BACKUP: Incorrect remote backup configuration format.")
